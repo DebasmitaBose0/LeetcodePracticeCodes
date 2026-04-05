b44643fb451555cddaba0192a1047354e09bd08d
DebasmitaBose0 <dbose272@gmail.com>
2026-04-05 16:06:07 +0530

    Solved

---
+++ 184. Department Highest Salary.sql
@@ -0,0 +1,11 @@
+SELECT Department, Employee, Salary
+FROM (
+    SELECT d.name AS Department,
+           e.name AS Employee,
+           e.salary AS Salary,
+           DENSE_RANK() OVER (PARTITION BY departmentId ORDER BY salary DESC) AS r
+    FROM Employee e
+    JOIN Department d
+    ON e.departmentId = d.id
+) t
+WHERE r = 1;
