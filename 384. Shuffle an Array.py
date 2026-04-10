839ab9d81303268e78e16d012ff0f16a6d3e84a9
Debasmita <dbose272@gmail.com>
2026-04-10 22:49:43 +0530

    Solved

---
+++ 384. Shuffle an Array.py
@@ -0,0 +1,27 @@
+import random
+
+class Solution:
+
+    def __init__(self, nums: List[int]):
+        self.original = list(nums)
+        self.array = nums
+
+    def reset(self) -> List[int]:
+        """
+        Resets the array to its original configuration and returns it.
+        """
+        self.array = list(self.original)
+        return self.array
+
+    def shuffle(self) -> List[int]:
+        """
+        Returns a random shuffling of the array.
+        """
+        # Fisher-Yates Shuffle
+        for i in range(len(self.array)):
+            # Pick a random index from i to the end of the array
+            swap_idx = random.randrange(i, len(self.array))
+            # Swap current element with the randomly picked element
+            self.array[i], self.array[swap_idx] = self.array[swap_idx], self.array[i]
+            
+        return self.array
