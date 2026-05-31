ef3b8a11682bb57806e4bfdf176b02dd876f43fc
Debasmita <dbose272@gmail.com>
2026-05-31 19:11:39 +0530

    Solved

---
+++ 2126. Destroying Asteroids.py
@@ -0,0 +1,14 @@
+class Solution:
+    def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
+        # Sort the asteroids in non-decreasing order
+        asteroids.sort()
+        
+        # Greedily collide with the smallest available asteroid
+        for asteroid in asteroids:
+            if mass >= asteroid:
+                mass += asteroid
+            else:
+                # If the planet can't destroy this asteroid, it's impossible
+                return False
+                
+        return True
