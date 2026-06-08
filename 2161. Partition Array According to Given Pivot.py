3283d9e446f480895eed3b11e535e18314d0414e
Debasmita <dbose272@gmail.com>
2026-06-08 19:40:58 +0530

    Solved

---
+++ 2161. Partition Array According to Given Pivot.py
@@ -0,0 +1,17 @@
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        less = []
+        equal = []
+        greater = []
+        
+        # Partition elements into three separate lists
+        for num in nums:
+            if num < pivot:
+                less.append(num)
+            elif num == pivot:
+                equal.append(num)
+            else:
+                greater.append(num)
+        
+        # Combine the lists to maintain the required relative order
+        return less + equal + greater
