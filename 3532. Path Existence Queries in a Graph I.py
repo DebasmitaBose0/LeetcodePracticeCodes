ebfb1957bb1c4818fad66a66b72d3ac1d705a306
Debasmita <dbose272@gmail.com>
2026-07-09 22:01:13 +0530

    Solved

---
+++ 3532. Path Existence Queries in a Graph I.py
@@ -0,0 +1,15 @@
+class Solution:
+    def pathExistenceQueries(self, n: int, nums: List[int], maxDiff: int, queries: List[List[int]]) -> List[bool]:
+        # component[i] will store the component ID for node i
+        component = [0] * n
+        curr_id = 0
+        
+        for i in range(1, n):
+            # If the gap between adjacent elements is too large, 
+            # they belong to different connected components
+            if nums[i] - nums[i-1] > maxDiff:
+                curr_id += 1
+            component[i] = curr_id
+            
+        # Answer each query in O(1) time
+        return [component[u] == component[v] for u, v in queries]
