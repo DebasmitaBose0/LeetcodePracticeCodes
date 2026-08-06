8e7f96a4a1ed533033d9f45d6c803b560c5e0a74
Debasmita <dbose272@gmail.com>
2026-08-06 21:49:10 +0530

    Solved

---
+++ 3345. Smallest Divisible Digit Product I.py
@@ -0,0 +1,13 @@
+class Solution:
+    def smallestNumber(self, n: int, t: int) -> int:
+        while True:
+            # Calculate the product of digits
+            prod = 1
+            for digit in str(n):
+                prod *= int(digit)
+            
+            # Check if the product is divisible by t
+            if prod % t == 0:
+                return n
+            
+            n += 1
